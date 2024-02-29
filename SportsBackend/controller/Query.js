0a037d73d50eb2ef62153faa269a27050eb9d3b9
lion656544 <100365269+lion656544@users.noreply.github.com>
2024-02-29 14:24:08 +0530

    edit

--- SportsBackend/controller/Query.js
+++ SportsBackend/controller/Query.js
@@ -6,17 +6,9 @@ const asyncHandler = require("express-async-handler");
 
 
 const createQuery = asyncHandler(async(req,res)=>{
-    const  {  email} = req.body;
-    const user = User.findOne({email});
-    if(user)
-    {
+    const  {  email} = req.body
         const newQuery = await Query.create(req.body);
         res.json(newQuery);
-    }
-    else
-    {
-        res.json("User Not Registered! You need to Sign Up First");
-    }
 })
 
 module.exports ={
